b95b597ae7fbdf6a857bbcbd548c843b41d889c5
Théo Cuisset <theo.cuisset@polytechnique.edu>
2024-04-11 15:47:43 +0200

    Fix issues of Madgraph Run2 LO genweights having issues

---
+++ python/genWeightLOFix.py
@@ -0,0 +1,26 @@
+""" Fix issues of Madgraph Run2 LO genweights having issues
+See https://twiki.cern.ch/twiki/bin/view/CMS/MCKnownIssues#Different_weights_at_LHE_level_f
+To be used only for Madgraph Run2 LO samples with no interference effects
+"""
+from analysis_tools.utils import import_root
+
+
+ROOT = import_root()
+
+
+class GenWeightLOFixRDFProducer():
+    def __init__(self, setGenWeightToOne):
+        self.setGenWeightToOne = setGenWeightToOne
+
+    def run(self, df):
+        if self.setGenWeightToOne:
+            df = df.Define("genWeightFixed", "1")
+        else:
+            df = df.Define("genWeightFixed", "genWeight")
+
+        return df, []
+
+def GenWeightLOFixRDF(*args, **kwargs):
+    setGenWeightToOne = kwargs.pop("setGenWeightToOne", False)
+
+    return lambda: GenWeightLOFixRDFProducer(setGenWeightToOne=setGenWeightToOne)
