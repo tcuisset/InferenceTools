b29c25a9c843e60f3f19a41303be3fdb626c7dbf
Théo Cuisset <theo.cuisset@polytechnique.edu>
2024-06-12 21:18:28 +0200

    Add muon SF ISO syst variation + electron RECO SFs (need to update Corrections as well)

--- python/dauIdIso.py
+++ python/dauIdIso.py
@@ -13,40 +13,47 @@ class dauIdIsoSFRDFProducer(JetLepMetSyst):
         self.isMC = kwargs.pop("isMC")
 
         if self.isMC:
-            default_systs = ["muon_id", "muon_iso", "ele_iso", "tau_vsjet", "tau_vse", "tau_vsmu"]
-            systnames = kwargs.pop("systs", ["central"] + default_systs)
-            template = ["" for i in range(len(default_systs))]
-            self.syst_names = []
-            self.systs = []
+            default_systs = ["muon_id", "muon_iso", "ele_reco", "ele_iso", "tau_vsjet", "tau_vse", "tau_vsmu"]
+            systnames = ["central"] + default_systs
+            base_template = ["" for i in range(len(default_systs))]
+            self.branch_names = []
+            """ Suffixes of branch names to go with branch_templates """
+            self.branch_templates = []
+            """ List of the set of suffixes to apply to input branches. Each outer list is a 6-element list [mu SF ID suffix, mu SF ISO suffix, ....]
+            for example ["", "syst_up", "", ...] for the mu SF ISO syst_up variation """
             for name in systnames:
                 if name == "central":
-                    self.syst_names.append("")
-                    self.systs.append(template)
+                    self.branch_names.append("")
+                    self.branch_templates.append(base_template)
                     continue
                 try:
                     ind = default_systs.index(name)
-                    for d in ["_up", "_down"]:
-                        tmp = copy(template)
-                        self.syst_names.append("_%s%s" % (name, d))
+                    dirs = kwargs.pop(name + "_syst_directions", ["_up", "_down"])
+                    for d in dirs:
+                        tmp = copy(base_template)
+                        self.branch_names.append("_%s%s" % (name, d))
                         tmp[ind] = d
-                        self.systs.append(tmp)
+                        self.branch_templates.append(tmp)
                 except ValueError:
                     raise ValueError("Systematic %s not available" % name)
                 
+            if len(kwargs) > 0:
+                print(f"### WARNING : dauIdIsoSFRDFProducer : remaining kwargs were not used : {kwargs}")
+            
             if not os.getenv("_dauIdIsoSF"):
                 os.environ["_dauIdIsoSF"] = "_dauIdIsoSF"
                 ROOT.gInterpreter.Declare("""
                     using Vfloat = const ROOT::RVec<float>&;
                     double get_dauIdIso_sf(
                             int pairType, int dau1_index, int dau2_index, Vfloat Tau_pt,
-                            Vfloat musf_id, Vfloat musf_reliso, Vfloat elesf, 
+                            Vfloat musf_id, Vfloat musf_reliso, Vfloat elesf_reco, Vfloat elesf_idiso, 
                             Vfloat Tau_sfDeepTau2017v2p1VSjet_pt, Vfloat Tau_sfDeepTau2017v2p1VSjet_dm,
                             Vfloat Tau_sfDeepTau2017v2p1VSe, Vfloat Tau_sfDeepTau2017v2p1VSmu) {
                         double idAndIsoSF_leg1 = 1.;
                         if (pairType == 0) {
                             idAndIsoSF_leg1 = musf_id.at(dau1_index) * musf_reliso.at(dau1_index);
                         } else if (pairType == 1) {
-                            idAndIsoSF_leg1 = elesf.at(dau1_index);
+                            idAndIsoSF_leg1 = elesf_reco.at(dau1_index) * elesf_idiso.at(dau1_index);
                         } else if (pairType == 2) {
                             if (Tau_pt[dau1_index] < 40)
                                 idAndIsoSF_leg1 = Tau_sfDeepTau2017v2p1VSjet_pt.at(dau1_index);
@@ -70,15 +77,16 @@ class dauIdIsoSFRDFProducer(JetLepMetSyst):
         if not self.isMC:
             return df, []
         branches = []
-        for name, syst_dir in zip(self.syst_names, self.systs):
-            df = df.Define("idAndIsoAndFakeSF%s" % name,
+        for branch_name, branch_template in zip(self.branch_names, self.branch_templates):
+            assert(len(branch_template) == 7)
+            df = df.Define("idAndIsoAndFakeSF%s" % branch_name,
                 "get_dauIdIso_sf(pairType, dau1_index, dau2_index, Tau_pt{0}, "
                     "musf_tight_id{1[0]}, musf_tight_reliso{1[1]}, "
-                    "elesf_wp80iso{1[2]}, Tau_sfDeepTau2017v2p1VSjet_pt_binned_Medium{1[3]}, "
-                    "Tau_sfDeepTau2017v2p1VSjet_dm_binned_Medium{1[3]}, "
-                    "Tau_sfDeepTau2017v2p1VSe_VVLoose{1[4]},"
-                    "Tau_sfDeepTau2017v2p1VSmu_VLoose{1[5]})".format(self.tau_syst, syst_dir))
-            branches.append("idAndIsoAndFakeSF%s" % name)
+                    "elesf_RecoAbove20{1[2]}, elesf_wp80iso{1[3]}, Tau_sfDeepTau2017v2p1VSjet_pt_binned_Medium{1[4]}, "
+                    "Tau_sfDeepTau2017v2p1VSjet_dm_binned_Medium{1[4]}, "
+                    "Tau_sfDeepTau2017v2p1VSe_VVLoose{1[5]},"
+                    "Tau_sfDeepTau2017v2p1VSmu_VLoose{1[6]})".format(self.tau_syst, branch_template))
+            branches.append("idAndIsoAndFakeSF%s" % branch_name)
 
         return df, branches
 
@@ -90,9 +98,12 @@ def dauIdIsoSFRDF(**kwargs):
     Required RDFModules: :ref:`HHLepton_HHLeptonRDF`, :ref:`Electron_eleSFRDF`,
     :ref:`Muon_muSFRDF`, :ref:`Tau_tauSFRDF`
 
-    :param systs: Systematics to be considered. Default: [``central``, ``muon_id``, ``muon_iso``,
-        ``ele_iso``, ``tau_vsjet``, ``tau_vse``, ``tau_vsmu``]. 
-    :type systs: list of str
+    Systematics considered : [``central``, ``muon_id``, ``muon_iso``,
+        ```ele_reco```, ``ele_iso``, ``tau_vsjet``, ``tau_vse``, ``tau_vsmu``]. 
+    
+    For choosing, for one of the systematics considered, which variations to use (on top of central) :
+    for example : muon_iso_syst_directions : default [_syst_up, _syst_down, _stat_up, _stat_down]
+    default _syst_directions : [_up, _down]
 
     YAML sintaxis:
 
@@ -103,7 +114,7 @@ def dauIdIsoSFRDF(**kwargs):
             path: Tools.Tools.dauIdIso
             parameters:
                 isMC: self.dataset.process.isMC
-                systs: [central, ...]
+                muon_iso_syst_directions: [_syst_up, _syst_down, _stat_up, _stat_down, ...]
 
     """
     return lambda: dauIdIsoSFRDFProducer(**kwargs)
