f4dc1460079998d2f3a357e3e042806c8002c35f
Elena Vernazza <e.vernazza1@campus.unimib.it>
2024-06-23 15:10:48 +0200

    Add GenPairType computation

--- python/ZZ_ZH_Analysis.py
+++ python/ZZ_ZH_Analysis.py
@@ -45,10 +45,16 @@ class BBTauTauFilterRDFProducer():
             ROOT.gInterpreter.Declare("""
                 using Vfloat = const ROOT::RVec<float>&;
                 using Vint   = const ROOT::RVec<int>&;
-                bool find_Zbb_Ztautau (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
-                    bool FoundSignal = false;
+                int GenPairType_Zbb_Ztautau (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
+
+                    int GenPairType = -1;
                     int n_b_fromZ = 0;
                     int n_tau_fromZ = 0;
+                    int tau1_id = -1;
+                    int tau2_id = -1;
+                    int dau1_gen_id = -1;
+                    int dau2_gen_id = -1;
+                    
                     for (int i_gen = 0; i_gen < GenPart_pdgId.size(); i_gen ++) {
                         if (GenPart_genPartIdxMother.at(i_gen) == -1) continue; // it is the incoming parton
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 5) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 23)) {
@@ -56,17 +62,40 @@ class BBTauTauFilterRDFProducer():
                         }
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 15) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 23)) {
                             n_tau_fromZ += 1;
+                            if (tau1_id == -1) tau1_id = i_gen;
+                            else if (tau2_id == -1) tau2_id = i_gen;
                         }
                     }
                     if ((n_b_fromZ > 1) && (n_tau_fromZ > 1)) {
-                        FoundSignal = true;
+                        for (int j_gen = 0; j_gen < GenPart_pdgId.size(); j_gen++) {
+                            if ((GenPart_genPartIdxMother.at(j_gen) == tau1_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau1_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                            else if ((GenPart_genPartIdxMother.at(j_gen) == tau2_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau2_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                        }
+                        if (((dau1_gen_id == 11) && (dau2_gen_id == 13)) || ((dau1_gen_id == 13) && (dau2_gen_id == 11))) GenPairType = 3; // decay modes not covered
+                        else if ((dau1_gen_id == 11) || (dau2_gen_id == 11)) GenPairType = 0;
+                        else if ((dau1_gen_id == 13) || (dau2_gen_id == 13)) GenPairType = 1;
+                        else GenPairType = 2;
+                        return GenPairType;
                     }
-                    return FoundSignal;
+                    else {
+                        return -1;
+                    }
                 }
-                bool find_Zbb_Htautau (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
-                    bool FoundSignal = false;
+                
+                int GenPairType_Zbb_Htautau (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
+
+                    int GenPairType = -1;
                     int n_b_fromZ = 0;
                     int n_tau_fromH = 0;
+                    int tau1_id = -1;
+                    int tau2_id = -1;
+                    int dau1_gen_id = -1;
+                    int dau2_gen_id = -1;
+
                     for (int i_gen = 0; i_gen < GenPart_pdgId.size(); i_gen ++) {
                         if (GenPart_genPartIdxMother.at(i_gen) == -1) continue; // it is the incoming parton
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 5) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 23)) {
@@ -74,17 +103,40 @@ class BBTauTauFilterRDFProducer():
                         }
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 15) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 25)) {
                             n_tau_fromH += 1;
+                            if (tau1_id == -1) tau1_id = i_gen;
+                            else if (tau2_id == -1) tau2_id = i_gen;
                         }
                     }
                     if ((n_b_fromZ > 1) && (n_tau_fromH > 1)) {
-                        FoundSignal = true;
+                        for (int j_gen = 0; j_gen < GenPart_pdgId.size(); j_gen++) {
+                            if ((GenPart_genPartIdxMother.at(j_gen) == tau1_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau1_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                            else if ((GenPart_genPartIdxMother.at(j_gen) == tau2_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau2_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                        }
+                        if (((dau1_gen_id == 11) && (dau2_gen_id == 13)) || ((dau1_gen_id == 13) && (dau2_gen_id == 11))) GenPairType = 3; // decay modes not covered
+                        else if ((dau1_gen_id == 11) || (dau2_gen_id == 11)) GenPairType = 0;
+                        else if ((dau1_gen_id == 13) || (dau2_gen_id == 13)) GenPairType = 1;
+                        else GenPairType = 2;
+                        return GenPairType;
                     }
-                    return FoundSignal;
+                    else {
+                        return -1;
+                    }
                 }
-                bool find_Ztautau_Hbb (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
-                    bool FoundSignal = false;
+
+                int GenPairType_Ztautau_Hbb (Vint GenPart_pdgId, Vint GenPart_genPartIdxMother) {
+
+                    int GenPairType = -1;
                     int n_b_fromH = 0;
                     int n_tau_fromZ = 0;
+                    int tau1_id = -1;
+                    int tau2_id = -1;
+                    int dau1_gen_id = -1;
+                    int dau2_gen_id = -1;
+
                     for (int i_gen = 0; i_gen < GenPart_pdgId.size(); i_gen ++) {
                         if (GenPart_genPartIdxMother.at(i_gen) == -1) continue; // it is the incoming parton
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 5) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 25)) {
@@ -92,12 +144,28 @@ class BBTauTauFilterRDFProducer():
                         }
                         if ((fabs(GenPart_pdgId.at(i_gen)) == 15) && (GenPart_pdgId.at(GenPart_genPartIdxMother.at(i_gen)) == 23)) {
                             n_tau_fromZ += 1;
+                            if (tau1_id == -1) tau1_id = i_gen;
+                            else if (tau2_id == -1) tau2_id = i_gen;
                         }
                     }
                     if ((n_b_fromH > 1) && (n_tau_fromZ > 1)) {
-                        FoundSignal = true;
+                        for (int j_gen = 0; j_gen < GenPart_pdgId.size(); j_gen++) {
+                            if ((GenPart_genPartIdxMother.at(j_gen) == tau1_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau1_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                            else if ((GenPart_genPartIdxMother.at(j_gen) == tau2_id) && ((fabs(GenPart_pdgId.at(j_gen)) == 11) || (fabs(GenPart_pdgId.at(j_gen)) == 13))) {
+                                dau2_gen_id = fabs(GenPart_pdgId.at(j_gen));
+                            }
+                        }
+                        if (((dau1_gen_id == 11) && (dau2_gen_id == 13)) || ((dau1_gen_id == 13) && (dau2_gen_id == 11))) GenPairType = 3; // decay modes not covered
+                        else if ((dau1_gen_id == 11) || (dau2_gen_id == 11)) GenPairType = 0;
+                        else if ((dau1_gen_id == 13) || (dau2_gen_id == 13)) GenPairType = 1;
+                        else GenPairType = 2;
+                        return GenPairType;
                     }
-                    return FoundSignal;
+                    else {
+                        return -1;
+                    }
                 }
             """)
 
@@ -107,19 +175,19 @@ class BBTauTauFilterRDFProducer():
             # define a new branch to check if it is or not a ZZ/ZH->bbtautau event
             if self.ProcType == "Zbb_Ztautau":
                 print(" ### Running bbtautau filter for Zbb_Ztautau")
-                df = df.Define("isBBTT", """find_Zbb_Ztautau(
+                df = df.Define("GenPairType", """GenPairType_Zbb_Ztautau(
                     GenPart_pdgId,
                     GenPart_genPartIdxMother
                 )""")
             elif self.ProcType == "Zbb_Htautau":
                 print(" ### Running bbtautau filter for Zbb_Htautau")
-                df = df.Define("isBBTT", """find_Zbb_Htautau(
+                df = df.Define("GenPairType", """GenPairType_Zbb_Htautau(
                     GenPart_pdgId,
                     GenPart_genPartIdxMother
                 )""")
             elif self.ProcType == "Ztautau_Hbb":
                 print(" ### Running bbtautau filter for Ztautau_Hbb")
-                df = df.Define("isBBTT", """find_Ztautau_Hbb(
+                df = df.Define("GenPairType", """GenPairType_Ztautau_Hbb(
                     GenPart_pdgId,
                     GenPart_genPartIdxMother
                 )""")
@@ -129,17 +197,18 @@ class BBTauTauFilterRDFProducer():
             # filter the events with ZZ/ZH->bbtautau
             if self.isSigBBTT:
                 # print(" ### DEBUG: isBBTT == 1")
-                df = df.Filter("isBBTT == 1", "BBTauTauFilterRDF")
+                df = df.Filter("GenPairType != -1", "BBTauTauFilterRDF")
             # filter the events without ZZ/ZH->bbtautau
             elif self.isBkgBBTT:
                 # print(" ### DEBUG: isBBTT == 0")
-                df = df.Filter("isBBTT == 0", "BBTauTauFilterRDF")
+                df = df.Filter("GenPairType == -1", "BBTauTauFilterRDF")
                 
-        return df, []
+        return df, ["GenPairType"]
 
 class BBTauTauFilterDummyRDFProducer():
     def run(self, df):
-        return df, []
+        df = df.Define("GenPairType", -1)
+        return df, ["GenPairType"]
 
 def BBTauTauFilterRDF(*args, **kwargs):
 
